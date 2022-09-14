1b9283fe161949ab0cf843101d660387ba2d3f4e
emailjtr <emailjtr@gmail.com>
2022-09-14 10:20:16 -0500

    sec15.72

---
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -0,0 +1,28 @@
+<?php
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch() {
+register_rest_route('university/v1', 'search', array(
+    'mehtods' => 'GET',
+    'callback' => 'universitySearchResults'
+));
+}
+
+function universitySearchResults() {
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+
+    $professorResults = array();
+
+    while($professors->have_posts()) {
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    }
+
+    return $professorResults;
+}
+?>
