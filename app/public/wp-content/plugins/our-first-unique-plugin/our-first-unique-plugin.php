236df1f9303d8f33d662f71d25dc35f0e753eadc
emailjtr <emailjtr@gmail.com>
2022-09-26 22:43:35 -0500

    sec23.118

---
+++ app/public/wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Test Plugin
+Description: Amazing
+Version: 1.0
+Author: JR
+*/
+
+class WordCountAndTimePlugin {
+    function __construct() {
+        add_action('admin_menu', array($this, 'adminPage'));
+        add_action('admin_init', array($this, 'settings'));
+    }
+
+    function settings() {
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+    }
+
+    function locationHTML() { ?>
+        <select name="wcp_location">
+            <option value="0">Start of post</option>
+            <option value="0">End of post</option>
+        </select>
+    <?php }
+
+    function adminPage() {
+    add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+}
+
+function ourHTML() { ?>
+    <div class="wrap">
+        <h1>Word Count Settings</h1>
+        <form action="options.php" method="POST">
+        <?php
+            settings_fields('wordcountplugin');
+            do_settings_sections('word-count-settings-page');
+            submit_button();
+        ?>
+        </form>
+    </div>
+<?php }
+}
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
