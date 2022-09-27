e42e35f59db8981bc6d3189988bf5c1f5e0199e0
emailjtr <emailjtr@gmail.com>
2022-09-26 21:32:05 -0500

    sec23.116

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -3,7 +3,7 @@
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>)"></div>
       <div class="page-banner__content container t-center c-white">
-      <h1 class="text-9xl font-bold underline">
+      <h1 class="text-8xl font-bold underline">
     Hello world!
   </h1>
         <h1 class="headline headline--large">Welcome!</h1>
